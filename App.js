43c5a0e1be93cc70748cc9f0b8c015a3e6646a57
Edison Lau <rness123@Edisons-MacBook-Air.local>
2024-04-05 14:13:34 -0400

    a6Node

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import express from 'express'
+import "dotenv/config";
+import mongoose from "mongoose";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
-// captial C here not sure why
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import UserRoutes from './Users/routes.js';
+import session from "express-session";
 import cors from "cors";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/kanbas';
+
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
-app.use(cors());
+
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
+
 app.use(express.json());
+
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
